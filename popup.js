9d461390e1a610de368eb661369686e7478f0b4d
bp2008 <bp2008@users.noreply.github.com>
2023-06-03 23:32:10 -0600

    Version 1.4: * Added qr code and options to switch between "amzn.com" and "amazon.com" and enable https linking.

--- popup.js
+++ popup.js
@@ -30,36 +30,57 @@ function CopyToClipboard(str)
 	document.execCommand('copy');
 	document.body.removeChild(el);
 }
+function CheckboxSetting(settingName, onChange)
+{
+	var ele_checkbox = document.getElementById(settingName);
+	ele_checkbox.addEventListener("change", function (e)
+	{
+		var obj = {};
+		obj[settingName] = e.target.checked;
+		chrome.storage.sync.set(obj);
+		if (typeof onChange === "function")
+			onChange();
+	});
+	chrome.storage.sync.get([settingName], function (result)
+	{
+		ele_checkbox.checked = result[settingName];
+	});
+	this.get = function ()
+	{
+		return ele_checkbox.checked;
+	}
+	this.set = function (value)
+	{
+		ele_checkbox.checked = !!value;
+	}
+}
 
 // Perform setup activities
-
 var ele_getLink = document.getElementById("getLink");
 var ele_myLink = document.getElementById("myLink");
 var ele_copied = document.getElementById("copied");
-var ele_copyAutomatically = document.getElementById("copyAutomatically");
 
-ele_copyAutomatically.addEventListener("change", function (e)
-{
-	chrome.storage.sync.set({ copyAutomatically: e.target.checked });
-});
-chrome.storage.sync.get(['copyAutomatically'], function (result)
-{
-	ele_copyAutomatically.checked = result.copyAutomatically;
-});
+var copyAutomatically = new CheckboxSetting("copyAutomatically");
+var useFullDomain = new CheckboxSetting("useFullDomain", produceShortLink);
+var useHttps = new CheckboxSetting("useHttps", produceShortLink);
 
-chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
+var lastUrl = null;
+
+function produceShortLink()
 {
-	var url = tabs[0].url;
-	var m = url.match(/\/(?:dp|gp\/product)\/([^\/?#]+)/i);
+	var m = lastUrl.match(/\/(?:dp|gp\/product)\/([^\/?#]+)/i);
 	if (!m)
 	{
-		console.log("Unexpected product URL format", url);
+		console.log("Unexpected product URL format", lastUrl);
 		ele_myLink.innerText = "URL not recognized as an Amazon product.";
 		ele_getLink.style.display = "none";
 		return;
 	}
 	var asin = m[1];
-	var link = "http://amzn.com/dp/" + asin;
+	var domain = "amzn.com";
+	if (useFullDomain.get())
+		domain = "amazon.com";
+	var link = "http" + (useHttps.get() ? "s" : "") + "://" + domain + "/dp/" + asin;
 	var doCopy = function ()
 	{
 		CopyToClipboard(link);
@@ -70,9 +91,22 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
 	ele_myLink.innerText = link;
 	SelectText("myLink");
 
-	chrome.storage.sync.get(['copyAutomatically'], function (result)
-	{
-		if (result.copyAutomatically)
-			doCopy();
+	if (copyAutomatically.get())
+		doCopy();
+
+	document.getElementById("qrcode").innerHTML = "";
+	var qrcode = new QRCode(document.getElementById("qrcode"), {
+		text: link,
+		width: 200,
+		height: 200,
+		colorDark: "#000000",
+		colorLight: "#ffffff",
+		correctLevel: QRCode.CorrectLevel.H
 	});
+}
+
+chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
+{
+	lastUrl = tabs[0].url;
+	produceShortLink();
 });
