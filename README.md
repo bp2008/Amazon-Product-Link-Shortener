072807d092215a6f485b484ed5649d74b84e545e
bp2008 <bp2008@users.noreply.github.com>
2024-09-27 08:37:54 -0600

    Version 1.8: * Try to support all amazon domains, not a hard-coded list anymore.

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A chrome extension which creates a short and clean link to the current amazon.co
 
 https://chrome.google.com/webstore/detail/amazon-product-link-short/kjjlkpmgccmimghapoichdfffocambjp
 
-Version 1.7 screenshot:
+Version 1.8 screenshot:
 [![screenshot](https://raw.githubusercontent.com/bp2008/Amazon-Product-Link-Shortener/master/screenshot.jpg)](https://chrome.google.com/webstore/detail/amazon-product-link-short/kjjlkpmgccmimghapoichdfffocambjp)
 
--- background.js
+++ background.js
@@ -4,42 +4,18 @@ chrome.runtime.onInstalled.addListener(() =>
 	chrome.action.disable();
 
 	// Clear all rules to ensure only our expected rules are set
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, () =>
-	{
-		let rule = {
-			conditions: [],
-			actions: [new chrome.declarativeContent.ShowAction()],
-		};
-		let domains = [
-			'amazon.ae',
-			'amazon.ca',
-			'amazon.cn',
-			'amazon.co.jp',
-			'amazon.co.uk',
-			'amazon.co.za',
-			'amazon.com',
-			'amazon.com.au',
-			'amazon.com.be',
-			'amazon.com.br',
-			'amazon.com.mx',
-			'amazon.com.tr',
-			'amazon.de',
-			'amazon.eg',
-			'amazon.es',
-			'amazon.fr',
-			'amazon.in',
-			'amazon.it',
-			'amazon.nl',
-			'amazon.pl',
-			'amazon.sa',
-			'amazon.se',
-			'amazon.sg'
-		];
-
-		for (let i = 0; i < domains.length; i++)
-		{
-			rule.conditions.push(new chrome.declarativeContent.PageStateMatcher({ pageUrl: { hostSuffix: domains[i] } }));
-		}
-		chrome.declarativeContent.onPageChanged.addRules([rule]);
-	});
+	    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        let rule = {
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        hostContains: '.amazon.',
+                        urlMatches: '^https://([^/.:]+\\.)*amazon(\\.[^/.:]{1,3}){1,2}/'
+                    }
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowAction()],
+        };
+        chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
 });
